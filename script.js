9b888ee8aaef9aa1955fe6b84faaec1d38b1ee5b
alisonstardust <zakulisami@o2.pl>
2018-05-29 22:27:56 +0200

    ES6 and css doodle

--- script.js
+++ script.js
@@ -1,52 +1,55 @@
-const input = document.querySelector('.non');
-const submitButton = document.querySelector(".submit");
-const forma = document.querySelector(".forma");
-const chewbaccaDiv = document.querySelector(".chewbacca");
-const print = document.querySelector(".print");
-const namePrint = document.querySelector(".namePrint");
-const apiAddress = 'https://swapi.co/api/people?search=';
+const startapp = () => {
+
+  const input = document.querySelector('.non');
+  const submitButton = document.querySelector(".submit");
+  const forma = document.querySelector(".forma");
+  const chewbaccaDiv = document.querySelector(".chewbacca");
+  const print = document.querySelector(".print");
+  const namePrint = document.querySelector(".namePrint");
+  const apiAddress = 'https://swapi.co/api/people?search=';
 
 
-forma.addEventListener('submit', functSubmit);
+  forma.addEventListener('submit', functSubmit);
 
-function functSubmit(event) {
-  event.preventDefault();
-  apirequest(input.value)
-}
+  function functSubmit(event) {
+    event.preventDefault();
+    apirequest(input.value)
+  }
 
-function apirequest(characterName) {
-  if (characterName === "wookie") {
-    alert('ArrRRrRrRRRrrR!')
-    chewbaccaDiv.style.display = "block";
-  } else {
-    fetch(apiAddress + characterName)
-      .then(resp => resp.json())
-      .then(getFilms)
-      .then(movieTitle => {
-        console.log(movieTitle)
-        if (movieTitle != undefined) {
-          print.innerText = movieTitle.toString( ).split(",").join(` 📽️ `);
-        } else {
-          console.log("What the hell")
-          print.innerText = `What do you mean by ${input.value} 🤔?`;
-        }
+  const apirequest = (characterName) => {
+    if (characterName === "wookie") {
+      alert('ArrRRrRrRRRrrR!')
+      chewbaccaDiv.style.display = "block";
+    } else {
+      fetch(apiAddress + characterName)
+        .then(resp => resp.json())
+        .then(getFilms)
+        .then(movieTitle => {
+          if (movieTitle != undefined) {
+            print.innerText = movieTitle.toString( ).split(",").join(` 📽️ `);
+          } else {
+            print.innerText = `What do you mean by ${input.value} 🤔?`;
+          }
+        })
+        .catch(error => console.log('Error: ', error));
+    }
+  }
+
+  const getFilms = (responseAgain) => {
+    console.log(responseAgain.count);
+    if (responseAgain.count !== 1) {
+      return undefined;
+    } else {
+      console.log(responseAgain.results[0].name)
+      namePrint.innerText = responseAgain.results[0].name;
+      const arrOfPromises = responseAgain.results[0].films.map(newApi => {
+        return fetch(newApi)
+          .then(movie => movie.json())
+          .then(bgy => bgy.title)
       })
-      .catch(error => console.log('Error: ', error));
+      return Promise.all(arrOfPromises);
+    }
   }
 }
 
-function getFilms(responseAgain) {
-  console.log(responseAgain.count);
-  if (responseAgain.count !== 1) {
-    return undefined;
-  } else {
-    console.log(responseAgain.results[0].name)
-    namePrint.innerText = responseAgain.results[0].name;
-    const arrOfPromises = responseAgain.results[0].films.map(newApi => {
-      return fetch(newApi)
-        .then(movie => movie.json())
-        .then(bgy => bgy.title)
-    })
-    return Promise.all(arrOfPromises);
-  }
-}
+startapp();
